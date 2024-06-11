43f529e044dbb0587280bb7e870c6ea8529d45a5
AdaiasMartins <110681069+AdaiasMartins@users.noreply.github.com>
2024-06-11 18:23:11 -0300

    configurando o Spring Security

---
+++ src/main/java/com/adaiasmartins/doacaodesangue/Doador/Security/ConfiguracoesDeSeguranca.java
@@ -0,0 +1,22 @@
+package com.adaiasmartins.doacaodesangue.Doador.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ConfiguracoesDeSeguranca {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf(Customizer.withDefaults())
+                .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
