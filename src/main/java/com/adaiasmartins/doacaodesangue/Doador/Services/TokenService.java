7653858a1f7c317785695ea25dae0dc18de5b2d0
AdaiasMartins <110681069+AdaiasMartins@users.noreply.github.com>
2024-06-12 13:57:58 -0300

    Escondendo token de acesso

--- src/main/java/com/adaiasmartins/doacaodesangue/Doador/Services/TokenService.java
+++ src/main/java/com/adaiasmartins/doacaodesangue/Doador/Services/TokenService.java
@@ -4,6 +4,7 @@ import com.adaiasmartins.doacaodesangue.Doador.Entities.Doador;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -12,10 +13,12 @@ import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String secret;
 
     public String gerarToken(Doador doador){
         try {
-            Algorithm algorithm = Algorithm.HMAC256("secret");
+            Algorithm algorithm = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withIssuer("auth0")
                     .withSubject(doador.getCpf())
